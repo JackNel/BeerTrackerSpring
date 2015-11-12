deb4c777cb7382e948009661ae4031985579e555
JackNel <jcneligan@gmail.com>
2015-11-12 11:44:52 -0500

    Use int instead of Integer in queryParams

--- src/main/java/com/theironyard/BeerTrackerController.java
+++ src/main/java/com/theironyard/BeerTrackerController.java
@@ -34,14 +34,13 @@ public class BeerTrackerController {
     }
 
     @RequestMapping("/")
-    public String home(HttpServletRequest request,
+    public String home(HttpSession session,
                        Model model,
                        String type,
                        Integer calories,
                        String search,
                        String showMine
     ) {
-        HttpSession session = request.getSession();
         String username = (String) session.getAttribute("username");
 
         if (username == null) {
@@ -67,9 +66,11 @@ public class BeerTrackerController {
     }
 
     @RequestMapping("/add-beer")
-    public String addBeer(String beername, String beertype, Integer beercalories, HttpServletRequest request) {
-        HttpSession session = request.getSession();
+    public String addBeer(String beername, String beertype, int beercalories, HttpSession session) throws Exception {
         String username = (String) session.getAttribute("username");
+        if (username == null) {
+            throw new Exception("Not logged in");
+        }
         User user = users.findOneByName(username);
 
         Beer beer = new Beer();
@@ -82,7 +83,11 @@ public class BeerTrackerController {
     }
 
     @RequestMapping("/edit-beer")
-    public String editBeer(Integer id, String name, String type) {
+    public String editBeer(int id, String name, String type, HttpSession session) throws Exception {
+        if (session.getAttribute("username") == null) {
+            throw new Exception("Not logged in");
+        }
+
         Beer beer = beers.findOne(id);
         beer.name = name;
         beer.type = type;
@@ -91,8 +96,7 @@ public class BeerTrackerController {
     }
 
     @RequestMapping("/login")
-    public String login(String username, String password, HttpServletRequest request) throws Exception {
-        HttpSession session = request.getSession();
+    public String login(String username, String password, HttpSession session) throws Exception {
         session.setAttribute("username", username);
 
         User user = users.findOneByName(username);
@@ -109,8 +113,7 @@ public class BeerTrackerController {
     }
 
     @RequestMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        HttpSession session = request.getSession();
+    public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/";
     }
